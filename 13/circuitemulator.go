4b10f3783dfdbe7c4f1623fd04b94146c17dbce3
prioritize <leach.jared@gmail.com>
2019-05-19 13:00:52 -0700

    Finished 13

--- 13/circuitemulator.go
+++ 13/circuitemulator.go
@@ -11,14 +11,14 @@ import (
 
 // Operation is an interface for operations that can be fed in through the input text file
 type Operation interface {
-	SetInputs(map[string]string)
-	CheckInputs() (Operation, bool)
-	RShift()
-	Or()
-	LShift()
-	And()
-	Not()
-	Assign() Operation
+	SetInputs(map[string]string) Operation
+	CheckInputs(m map[string]string) (Operation, bool)
+	RShift(m map[string]string) Operation
+	Or(m map[string]string) Operation
+	LShift(m map[string]string) Operation
+	And(m map[string]string) Operation
+	Not(m map[string]string) Operation
+	Assign(m map[string]string) Operation
 	GetOp() string
 }
 
@@ -45,94 +45,121 @@ type Assign struct {
 }
 
 //RShift needs two inputs, a value and a offset and provides an output
-func (g Gate) RShift() {
+func (g Gate) RShift(m map[string]string) Operation {
 	if g.outputBool[0] == false {
 		g.output[0] = g.inputs[0] >> g.inputs[1]
 		g.outputBool[0] = true
 	}
+	m[g.mapOutput[0]] = strconv.Itoa(int(g.output[0]))
+	return g
 }
 
 //LShift needs two inputs, a value and a offset and provides an output
-func (g Gate) LShift() {
+func (g Gate) LShift(m map[string]string) Operation {
 	if g.outputBool[0] == false {
 		g.output[0] = g.inputs[0] << g.inputs[1]
 		g.outputBool[0] = true
 	}
+	m[g.mapOutput[0]] = strconv.Itoa(int(g.output[0]))
+	return g
 }
 
 // And needs two inputs, two values and provides an output
-func (g Gate) And() {
+func (g Gate) And(m map[string]string) Operation {
 	if g.outputBool[0] == false {
 		g.output[0] = g.inputs[0] & g.inputs[1]
 		g.outputBool[0] = true
 	}
+	m[g.mapOutput[0]] = strconv.Itoa(int(g.output[0]))
+	return g
 }
 
 // Or needs two inputs, two values and provides an output
-func (g Gate) Or() {
+func (g Gate) Or(m map[string]string) Operation {
 	if g.outputBool[0] == false {
 		g.output[0] = g.inputs[0] | g.inputs[1]
 		g.outputBool[0] = true
 	}
+	m[g.mapOutput[0]] = strconv.Itoa(int(g.output[0]))
+	return g
 }
 
 // Not is not implemented as it is not required
-func (g Gate) Not() {
+func (g Gate) Not(m map[string]string) Operation {
 	// Not Implemented. Not is an Assign function
+	return g
 }
 
 // Assign is not implemented as it is not required
-func (g Gate) Assign() Operation {
+func (g Gate) Assign(m map[string]string) Operation {
 	// Not implemented. Assign is an Assign function
 	return g
 }
 
 // CheckInputs loops through the valuesSet slice and checks for any falses. If false, return false, else return true
-func (g Gate) CheckInputs() (Operation, bool) {
+func (g Gate) CheckInputs(m map[string]string) (Operation, bool) {
 	// TODO: Loop through the valuesSet bool, if all all are true, return true
 	// TODO: Can perform some error checking here and check the values in the map to ensure no errors  were made
+	var b Operation
 	for _, v := range g.valuesSet {
 		if v == false {
 			return g, false
 		}
 	}
-	return g, true
+	switch {
+	case g.op[0] == "RSHIFT":
+		b = g.RShift(m)
+	case g.op[0] == "LSHIFT":
+		b = g.LShift(m)
+	case g.op[0] == "AND":
+		b = g.And(m)
+	case g.op[0] == "OR":
+		b = g.Or(m)
+	}
+	return b, true
 }
 
 // RShift is not implemented as it is not required
-func (a Assign) RShift() {
+func (a Assign) RShift(m map[string]string) Operation {
 	//Not implemented. RShift is a Gate function
+	return a
 }
 
 // LShift is not implemented as it is not required
-func (a Assign) LShift() {
+func (a Assign) LShift(m map[string]string) Operation {
 	// Not implemented. LShift is a Gate function
+	return a
 }
 
 // And is not implemented as it is not required
-func (a Assign) And() {
+func (a Assign) And(m map[string]string) Operation {
 	// Not implemented. LShift is a Gate function
+	return a
 }
 
 // Or is not implemented as it is not required
-func (a Assign) Or() {
+func (a Assign) Or(m map[string]string) Operation {
 	// Not implemented. LShift is a Gate function
+	return a
 }
 
 // Not requires one input. It provides one output
-func (a Assign) Not() {
+func (a Assign) Not(m map[string]string) Operation {
 	if a.outputBool[0] == false {
 		a.output[0] = ^a.inputs[0]
 		a.outputBool[0] = true
 	}
+	m[a.mapOutput[0]] = strconv.Itoa(int(a.output[0]))
+	return a
 }
 
 // Assign requires one input. It provides one output
-func (a Assign) Assign() Operation {
+func (a Assign) Assign(m map[string]string) Operation {
 	if a.outputBool[0] == false {
 		a.output[0] = a.inputs[0]
 		a.outputBool[0] = true
 	}
+	m[a.mapOutput[0]] = strconv.Itoa(int(a.output[0]))
 	return a
 }
 
@@ -147,7 +174,7 @@ func (a Assign) GetOp() string {
 }
 
 // CheckInputs loops through all inputBool values (valuesSet) and if all return true returns true
-func (a Assign) CheckInputs() (Operation, bool) {
+func (a Assign) CheckInputs(m map[string]string) (Operation, bool) {
 	// TODO: Loop through the valuesSet bool, if all all are true, return true
 	// TODO: Can perform some error checking here and check the values in the map to ensure no errors  were made
 	var b Operation
@@ -159,13 +186,15 @@ func (a Assign) CheckInputs() (Operation, bool) {
 	// TODO: Consider executing the operation here and copying the calculated value into it's location in the dictionary
 	switch {
 	case a.op[0] == "ASSIGN":
-		b = a.Assign()
+		b = a.Assign(m)
+	case a.op[0] == "NOT":
+		b = a.Not(m)
 	}
 	return b, true
 }
 
 // SetInputs checks the values in the map and if they are integers places them into the input values in the RShift object
-func (g Gate) SetInputs(m map[string]string) {
+func (g Gate) SetInputs(m map[string]string) Operation {
 	// Get the values from the map to check if they are integers
 	for i, v := range g.mapInputs {
 		elem := m[v]
@@ -175,10 +204,11 @@ func (g Gate) SetInputs(m map[string]string) {
 			g.valuesSet[i] = true
 		}
 	}
+	return g
 }
 
 // SetInputs checks the values in the map and if they are integers places them into the input values in the RShift object
-func (a Assign) SetInputs(m map[string]string) {
+func (a Assign) SetInputs(m map[string]string) Operation {
 	// Get the values from the map to check if they are integers
 	for i, v := range a.mapInputs {
 		elem := m[v]
@@ -188,6 +218,7 @@ func (a Assign) SetInputs(m map[string]string) {
 			a.valuesSet[i] = true
 		}
 	}
+	return a
 }
 
 // MakeGate processes a line and returns a Gate
@@ -280,12 +311,16 @@ func main() {
 
 	// TODO: Return nodes from all the Gate and Assign functions to enable storing of the values in the object slice
 	// TODO: Add function that checks for truth in the outputBool, if true, set the output value into the key value in the map
-	for i, v := range nodeSlice {
-		node, check := v.CheckInputs()
-		if check == true {
-			nodeSlice[i] = node
-			fmt.Println(node)
-			fmt.Println(nodeSlice[i])
+
+	for x := 0; x < 1000; x++ {
+		for i, v := range nodeSlice {
+			v = v.SetInputs(nodeValues)
+			node, check := v.CheckInputs(nodeValues)
+			if check == true {
+				nodeSlice[i] = node
+				// TODO: Write a function get to the output node OR handle the setting of the key value in a separate function which will require
+				// TODO: passing of the nodeValues map
+			}
 		}
 	}
 	// TODO: Build the dictionary of objects
