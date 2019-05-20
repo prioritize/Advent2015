9e85f37c696b37a98e756a8426a86a4273487238
Jared Leach <leach.jared@gmail.com>
2019-05-20 16:04:31 -0700

    Finished 15

--- 15/literalvsactual.go
+++ 15/literalvsactual.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -21,10 +23,41 @@ func main() {
 		if e == io.EOF {
 			break
 		}
-		byteSlice = append(byteSlice, in)
+		if in == byte(' ') || in == byte('\n') {
+			continue
+		} else {
+			byteSlice = append(byteSlice, in)
+		}
+
 	}
 	fmt.Println(len(byteSlice))
 
-	fileRead.Reset()
-
+	nextFile, e := os.Open("input.txt")
+	if e != nil {
+		panic(e)
+	}
+	newFileReader := bufio.NewReader(nextFile)
+	stringSlice := make([]string, 0)
+	for {
+		inString, err := newFileReader.ReadBytes('\n')
+		stringSlice = append(stringSlice, string(inString))
+		if err == io.EOF {
+			break
+		}
+	}
+	outSlice := make([]string, 0)
+	for _, v := range stringSlice {
+		v = strings.Replace(v, "\n", "", -1)
+		v, e = strconv.Unquote(v)
+		outSlice = append(outSlice, v)
+	}
+	count := 0
+	for _, j := range outSlice {
+		for range j {
+			count++
+		}
+	}
+	fmt.Println(outSlice)
+	fmt.Println(count)
+	fmt.Println(len(byteSlice) - count)
 }
