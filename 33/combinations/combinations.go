32b59cb81cfcfd30b7d323caf324d21f1dd06a47
prioritize <leach.jared@gmail.com>
2019-05-28 20:54:30 -0700

    Finished 33

--- 33/combinations/combinations.go
+++ 33/combinations/combinations.go
@@ -6,25 +6,24 @@ import (
 )
 
 type Combination struct {
-	set     [][]string
-	initial []string
+	set     [][]int
+	initial []int
 }
 
-func New(initial []string) Combination {
+func New(initial []int) Combination {
 	var c Combination
 	c.initial = initial
-	c.set = make([][]string, 0)
+	c.set = make([][]int, 0)
 	for i := range c.set {
-		c.set[i] = make([]string, 0)
+		c.set[i] = make([]int, 0)
 	}
 	return c
 }
 func (c *Combination) GenerateSet() {
 	combos := uint(math.Pow(2, float64(len(c.initial))))
-	fmt.Println(combos)
 	var i uint
 	for i < combos {
-		currentCombo := make([]string, 0)
+		currentCombo := make([]int, 0)
 		for j, x := range c.initial {
 			if i&(1<<uint(j)) != 0 {
 				currentCombo = append(currentCombo, x)
@@ -33,5 +32,22 @@ func (c *Combination) GenerateSet() {
 		c.set = append(c.set, currentCombo)
 		i++
 	}
-	fmt.Println(c.set)
+}
+func (c Combination) CheckSet() int {
+	matches := 0
+	minCount := 0
+	for _, v := range c.set {
+		total := 0
+		for _, j := range v {
+			total += j
+		}
+		if total == 150 {
+			matches++
+			if len(v) == 4 {
+				minCount++
+			}
+		}
+	}
+	fmt.Println(minCount)
+	return matches
 }
